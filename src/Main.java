5b151db31f46277cc9d84d183d3938295c31eea5
Timofey <timofey.sharipov@gmail.com>
2023-10-30 20:48:32 +0300

    check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        String login = "skypro5";
+        String password = "qwerty123";
+        String confirmPassword = "qwerty123";
+        try {
+            Validator.validate(login, password, confirmPassword);
+            System.out.println("All ok");
+        } catch (WrongLoginException | WrongPasswordException exception) {
+            exception.printStackTrace();
+        }//я решил использовать этот способ по причине того что мне кажется такой код более читаем,что я считаю важным
     }
 }
--- src/Validator.java
+++ src/Validator.java
@@ -1,22 +1,48 @@
 import exception.WrongLoginException;
+import exception.WrongPasswordException;
 
 public class Validator {
-    private static final String LEGAL_LOGIN_CHARACTERS =
+    private static final String LEGAL_CHARACTERS =
             "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM_";
-    private static final int LOGIN_MAX_LENGTH = 20 ;
+    private static final int MAX_LENGTH = 20 ;
     public static void validate (
             String login,
             String password,
             String confirmPassword
     ) {
+        validateLogin(login);
+        validatePassword(password, confirmPassword);
+    }
+
+    private static void validateLogin(String login) {
         for (int i = 0; i < login.length(); ++i) {
-            if (LEGAL_LOGIN_CHARACTERS.indexOf (login.charAt(i)) == -1) {
-                throw new WrongLoginException ("Логин содержит недопустимые символы");
+            if (LEGAL_CHARACTERS.indexOf(login.charAt(i)) == -1) {
+                throw new WrongLoginException("Логин содержит недопустимые символы");
             }
         }
 
-        if (login.length() > LOGIN_MAX_LENGTH) {
+        if (login.length() > MAX_LENGTH) {
             throw new WrongLoginException("Превышена допустимая длина логина");
+        }
+    }
+
+    private static void validatePassword (
+            String password,
+            String confirmPassword
+    ) {
+
+        for (int i = 0; i < password.length(); ++i) {
+            if (LEGAL_CHARACTERS.indexOf (password.charAt(i)) == -1) {
+                throw new WrongPasswordException("Пароль содержит недопустимые символы");
+            }
+        }
+
+        if (password.length() > MAX_LENGTH) {
+            throw new WrongPasswordException("Превышена допустимая длина пароля");
+        }
+
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException ("Пароли не совпадают");
         }
     }
 }
